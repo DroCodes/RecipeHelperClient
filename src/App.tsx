cc212b85ebb985ae6ef81cf78b4af339556f8160
DroCodes <101131393+DroCodes@users.noreply.github.com>
2022-12-14 23:30:31 -0600

    cleaned out unused code

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     const [recipes, setRecipes]: any = useState([]);
     const [recipeData, setRecipeData] = useState([]);
     // const imgURL = "http://localhost:8080";
-    const imgURL = "https://recipe-guru.herokuapp.com/static/";
+    const imgURL = "https://recipe-guru.herokuapp.com/static";
 
     function useRecipeData(r: []) {
         setRecipeData(r);
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Home = (props: { recipe: []; getRecipeData: any; imgURL: string }) => {
